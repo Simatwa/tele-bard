9d0936c33954dd0c68ca1c7ed8779f76e5cfee9e
Simatwa <smartwacaleb@gmail.com>
2023-12-09 14:48:32 +0300

    general fix

--- main.py
+++ main.py
@@ -1,34 +1,115 @@
 from Bard import Chatbot
 import os
 import telebot
+from telebot import types
 from dotenv import load_dotenv
 
-load_dotenv('.env')
+load_dotenv(".env")
 
-get_key = lambda key : os.environ.get(key)
+get_key = lambda key: os.environ.get(key)
 
-bard = Chatbot(get_key('bard'))
+bard = Chatbot(get_key("bard"))
 
-bot = telebot.TeleBot(get_key('telebot'))
+bot = telebot.TeleBot(get_key("telebot"))
+
+default_user_id = get_key("userId")
+
+
+def is_verified(message):
+    """Ensures it serves the right user"""
+    return str(message.from_user.id) == default_user_id
+
+
+def anonymous_user(message):
+    """Generate message for strange user"""
+    response = f"""
+	Hi **{message.from_user.first_name}**.
+	You are not authorised to use this Bot.
+	I recommmend setting up your own bot like this.
+	Get the sourcecodes from [github](https://github.com/Simatwa/tele-bard).
+	"""
+    return response.strip()
+
 
 def generate_response(text):
-	try:
-		return bard.ask(text).get('content')
-	except Exception as e:
-		return e.args[1] if len(e.args) >1 else e
-		
-@bot.message_handler(commands=['start','help'])
+    """Fetches response from Bard"""
+    try:
+        return bard.ask(text).get("content")
+    except Exception as e:
+        return e.args[1] if len(e.args) > 1 else e
+
+
+@bot.message_handler(commands=["start", "help"])
 def display_help(message):
-	help_message=f"""
-Hi **{message.from_user.first_name}**.
-This bot will allow you to directly chat with [GoogleBard](https://bard.google.com).
-Hope you will enjoy.
-	"""
-	bot.reply_to(message, help_message, parse_mode="Markdown")
+    sender = message.from_user.first_name
+    help_message = (
+        f"""
+	Hi **{sender}**.This bot will allow you to directly chat with **GoogleBard**.
+	Hope you will enjoy.
 	
-@bot.message_handler(func = lambda m:True)
+	> Available commands
+	
+	/start or /help : Show this messsage
+	
+	/myId : Check your Telegram's ID
+	/reset : Start new conversation
+	
+	Any other text interacts with Bard.
+   
+   """.strip()
+        if is_verified(message)
+        else anonymous_user(message)
+    )
+    bot.reply_to(message, help_message, parse_mode="Markdown")
+
+
+@bot.message_handler(commands=["reset"])
+def reset_conversation(message):
+    if is_verified(message):
+        markup = types.InlineKeyboardMarkup()
+        yes_btn = types.InlineKeyboardButton("Yes!", callback_data="yes")
+        no_btn = types.InlineKeyboardButton("No!", callback_data="no")
+        markup.add(yes_btn, no_btn)
+        bot.send_message(
+            message.chat.id,
+            text="Are you sure to reset conversation",
+            reply_markup=markup,
+        )
+    else:
+        bot.reply_to(message, anonymous_user(message), parse_mode="Markdown")
+
+
+@bot.message_handler(commands=["myId"])
+def user_id(message):
+    """Get to know your id"""
+    bot.reply_to(
+        message,
+        text=f"Your Telegram ID is **{message.from_user.id}**",
+        parse_mode="Markdown",
+    )
+
+
+@bot.message_handler(func=lambda m: True)
 def chat_with_bard(message):
-	bot.reply_to(message,generate_response(message.text), parse_mode="Markdown")
-	
-if __name__=="__main__":
-	bot.infinity_polling()
+    """Generate response with Bard"""
+    if is_verified(message):
+        bot.reply_to(message, generate_response(message.text), parse_mode="Markdown")
+    else:
+        bot.reply_to(message, anonymous_user(message), parse_mode="Markdown")
+
+
+@bot.callback_query_handler(func=lambda message: True)
+def callback_query(call):
+    if call.data == "yes":
+        global bard
+        bard = Chatbot(get_key("bard"))
+        bot.send_message(call.message.chat.id, "Conversation reset successfully.")
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Conversation retained.")
+
+    else:
+        bot.send_message(call.message.chat.id, f"Unrecognized command - {call.data}")
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
